8d119f5abd12bf616cba62aff90d7e3c2ef8113f
Arthur <quiquearthur5@gmail.com>
2018-10-12 11:21:39 +0200

    Nouveau

--- MiniGameProject/src/games/Home.java
+++ MiniGameProject/src/games/Home.java
@@ -1,9 +1,35 @@
 package games;
 
-public class Home {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+
+public class Home extends Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Application.launch(Home.class, args);
+		
+		
+		
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		primaryStage.setTitle("Des Chiffres et Des Lettres");
+		Group root = new Group();
+        Scene scene = new Scene(root, 1200, 720, Color.LIGHTGREY);
+        MainMenu menu = new MainMenu();
+        primaryStage.setScene(scene);
+        
+        
+        
+                
+        primaryStage.show();
 
 	}
 
--- MiniGameProject/src/games/Sudoku.java
+++ MiniGameProject/src/games/Sudoku.java
@@ -2,4 +2,35 @@ package games;
 
 public class Sudoku {
 
+	protected Grid G;
+	protected int Gsize;	//Taille de la grille
+	protected int Rsize;    //Taille de la région
+	
+	
+	public Sudoku() {
+	}
+	
+	public void GenererGrille() {
+		
+	}
+	
+	public int VerifieUnicite() {
+		return 0;
+	}
+	
+	public void GridDisplay() {
+		
+	}
+	
+	public void SaisirunCoup() {
+		
+	}
+	
+	public void JouerUnCoup() {
+		
+	}	
+	
+	public void AnnulerUnCoup() {
+		
+	}
 }

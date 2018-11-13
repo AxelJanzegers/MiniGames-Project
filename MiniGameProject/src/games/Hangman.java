6cec2ae25ece3a984a1ad7b9e8637d506ed0c9fb
Matthieu905 <sakamisare@gmail.com>
2018-11-13 15:34:13 +0100

    AAAAAA

--- MiniGameProject/src/games/Hangman.java
+++ MiniGameProject/src/games/Hangman.java
@@ -1,5 +1,10 @@
 package games;
+import javax.swing.JFrame;
 
 public class Hangman {
-
+	 
+	public class Test {     
+	    JFrame fenetre = new JFrame();      
+	}
+int nbre = (int)(Math.random()*336529);
 }

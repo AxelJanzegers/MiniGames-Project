6776afacfa7b17e47f9cbea47f5d472bf7ff87b1
Arthur <quiquearthur5@gmail.com>
2018-11-13 17:08:17 +0100

    13112018

--- MiniGameProject/src/display/Home.java
+++ MiniGameProject/src/display/Home.java
@@ -1,9 +1,13 @@
 package display;
 
+
 import games.Hangman;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -12,12 +16,10 @@ public class Home extends Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		//Application.launch(Home.class, args);
-		
-		Hangman h = new Hangman();
-		h.startGame();
-			
+
+		Application.launch(Home.class, args);
+
+
 	}
 
 	@Override
@@ -25,10 +27,25 @@ public class Home extends Application {
 		// TODO Auto-generated method stub
 		primaryStage.setTitle("Des Chiffres et Des Lettres");
 		Group root = new Group();
-        Scene scene = new Scene(root, 1200, 720, Color.LIGHTGREY);
-       // MainMenu menu = new MainMenu();
-        primaryStage.setScene(scene);    
-        primaryStage.show();
+		Scene scene = new Scene(root, 1200, 720, Color.LIGHTGREY);
+		Button btn = new Button();
+
+
+		btn.setLayoutX(500);
+		btn.setLayoutY(380);
+		btn.setText("Pendu");
+		btn.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				Hangman h = new Hangman();
+				h.startGame();
+			}
+		});
+
+		root.getChildren().add(btn);
+		// MainMenu menu = new MainMenu();
+
+		primaryStage.setScene(scene);    
+		primaryStage.show();
 	}
 
-}
+} s

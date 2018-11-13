427efd84c22686d3d2206bc1e0b40933f9c34382
Arthur <quiquearthur5@gmail.com>
2018-11-13 17:33:55 +0100

    131118

--- MiniGameProject/src/display/Home.java
+++ MiniGameProject/src/display/Home.java
@@ -1,9 +1,7 @@
 package display;
 
-<<<<<<< HEAD
-=======
+
 import java.io.File;
->>>>>>> 110b683fa9cd9fe4769bc67f47af3d2e129e393b
 
 import games.Hangman;
 import javafx.application.Application;
@@ -21,7 +19,6 @@ public class Home extends Application {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-<<<<<<< HEAD
 		Application.launch(Home.class, args);
 
 
@@ -53,31 +50,5 @@ public class Home extends Application {
 		primaryStage.show();
 	}
 
-} s
-=======
-		//Application.launch(Home.class, args);
+}  
 
-		// Hangman h = new Hangman();
-		// h.startGame();
-
-		Grid grille = new Grid();
-		grille.afficheGrille();
-
-	} 
-
-
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        // TODO Auto-generated method stub
-        primaryStage.setTitle("Des Chiffres et Des Lettres");
-        Group root = new Group();
-        Scene scene = new Scene(root, 1200, 720, Color.LIGHTGREY);
-       // MainMenu menu = new MainMenu();
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    
-}
->>>>>>> 110b683fa9cd9fe4769bc67f47af3d2e129e393b
